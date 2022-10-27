4ec05e8f98af5642e5c1ffe97f7ca0d31bbc42d5
Xurxo Martínez Ferreira <xurxolaxe@gmail.com>
2022-10-27 12:36:46 +0200

    Erro checkForm() e implementado notasAlumnos()
    En checkForm solo checkeaba que as notas foran int, agora checkea que sexa int ou float.
    notasAlumnos() é a función que crea os arrays de media, aprobados... e o de suspensos e aprobados por alumno

--- controllers/notas.xurxo.controller.php
+++ controllers/notas.xurxo.controller.php
@@ -3,6 +3,69 @@
 if (isset($_POST["enviar"])) {
     $data["err"] = checkForm($_POST);
     $data["in"] = filter_var_array($_POST, FILTER_SANITIZE_SPECIAL_CHARS);
+    if (empty($data["err"]["notas"])) {
+        $data["res"] = notasAlumnos($_POST);
+        var_dump($data);
+    }
+}
+
+function notasAlumnos(array $POST): array
+{
+    $json = json_decode($_POST["notas"], true);
+
+    $datos = [];
+    $cualificacions = [];
+
+    foreach ($json as $asignatura => $alumnos) {
+        $notasMedia = 0;
+        $cantAlumnos = 0;
+        $suspensos = 0;
+        $aprobados = 0;
+        $max = ["nome" => "", "nota" => 0];
+        $min = ["nome" => "", "nota" => 10];
+
+        foreach ($alumnos as $nome => $arNotas) {
+            $sumaNotasAlumno = 0;
+            $cantNotasAlumno = 0;
+
+            foreach ($arNotas as $nota) {
+                $sumaNotasAlumno += $nota;
+                $cantNotasAlumno++;
+            }
+            $nota = $sumaNotasAlumno / $cantNotasAlumno;
+
+            $notasMedia += $nota;
+            $cantAlumnos++;
+
+            if (!isset($cualificacions[$nome])) {
+                $cualificacions[$nome] = ["aprobadas" => 0, "suspensas" => 0];
+            }
+            if ($nota < 5) {
+                $cualificacions[$nome]["suspensas"]++;
+                $suspensos++;
+            } else if ($nota >= 5) {
+                $cualificacions[$nome]["aprobadas"]++;
+                $aprobados++;
+            }
+            if ($max["nota"] < $nota) {
+                $max["nome"] = $nome;
+                $max["nota"] = $nota;
+            }
+            if ($min["nota"] > $nota) {
+                $min["nome"] = $nome;
+                $min["nota"] = $nota;
+            }
+        }
+
+        $datos[$asignatura] = [
+            "media" => number_format(($notasMedia / $cantAlumnos), 2, ",", "."),
+            "suspensos" => $suspensos,
+            "aprobados" => $aprobados,
+            "max" => $max,
+            "min" => $min
+        ];
+    }
+    return ["datos" => $datos, "cualificacions" => $cualificacions];
 }
 
 function checkForm(array $POST): array
@@ -30,7 +93,7 @@ function checkForm(array $POST): array
                             array_push($errNotas, "As notas de '" . htmlentities($nome) . "' non están en formato array.");
                         } else {
                             foreach ($arNotas as $nota) {
-                                if (!is_int($nota)) {
+                                if (!is_float($nota) && !is_int($nota)) {
                                     array_push($errNotas, "O alumno '" . htmlentities($nome) . "' en '" . htmlentities($asignatura) . "' contén unha nota que non é un número.");
                                 }
                             }
